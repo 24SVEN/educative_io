fa241c233daf513f66af094c57645470ec16703c
24SVEN <johnahn0124@gmail.com>
2022-03-15 12:37:20 -0400

    hash map method

--- xor/find_single_number.py
+++ xor/find_single_number.py
@@ -1,9 +1,20 @@
 def find_single_number(arr):
     # TODO: Write your code here
-    bit_val = 0
+    # bit_val = 0
+    # for i in range(len(arr)):
+    #     bit_val = bit_val ^ arr[i]
+    # return bit_val
+
+    dic_bit = {}
+
     for i in range(len(arr)):
-        bit_val = bit_val ^ arr[i]
-    return bit_val
+        if arr[i] in dic_bit:
+            dic_bit[arr[i]] -= 1
+            del dic_bit[arr[i]]
+        else:
+            dic_bit[arr[i]] = 1
+
+    return [key for key in dic_bit.keys()][0]
 
 def main():
     arr = [1, 4, 2, 1, 3, 2, 3]
