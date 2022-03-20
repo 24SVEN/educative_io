a0256744ae5c92f06b0df3de05a700fddd1f9c2b
John Ahn <Ahn_John@bah.com>
2022-03-20 18:59:30 -0400

    inserted instead of sorting for merge interval

--- merged_intervals/insert_intervals.py
+++ merged_intervals/insert_intervals.py
@@ -1,9 +1,19 @@
 def insert(intervals, new_interval):
     merged = []
     # TODO: Write your code here
-    intervals.append(new_interval)
+    #intervals.append(new_interval)
 
-    intervals.sort(key=lambda x:x[0])
+    #intervals.sort(key=lambda x:x[0])
+
+    #already sorted
+    for i in range(len(intervals)):
+        if new_interval[0] > intervals[i][0]:
+            insert_index = i
+            break
+    
+    
+    intervals = intervals.copy()[:i+1] + [new_interval] + intervals.copy()[i+1:]
+
 
     idx = 0
 
@@ -20,8 +30,8 @@ def insert(intervals, new_interval):
 
 def main():
   print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
-  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
-  print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
+#   print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
+#   print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
 
 
 main()
