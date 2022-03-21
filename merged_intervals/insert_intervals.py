67f061fa30d84ac77fd302fc7b986afb064d6b67
John Ahn <Ahn_John@bah.com>
2022-03-20 20:27:57 -0400

    created dynamic programming folder

--- merged_intervals/insert_intervals.py
+++ merged_intervals/insert_intervals.py
@@ -30,8 +30,8 @@ def insert(intervals, new_interval):
 
 def main():
   print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
-#   print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
-#   print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
+  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
+  print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
 
 
 main()
