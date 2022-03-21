1e9381674b0ac1a613703e32d7bdb92b1d5d3878
24SVEN <johnahn0124@gmail.com>
2022-03-21 02:31:28 -0400

    solved initially without realizing each fruit is used once

---
+++ dynamic_programming/knapsack.py
@@ -0,0 +1,35 @@
+
+class Solution():
+    def solve_knapsack(self,profits, weights, capacity):
+        # TODO: Write your code here
+
+        dp = [[0 for i in range(capacity+1)] for _ in range(len(weights))]
+
+        for row in range(len(weights)):
+            for col in range(capacity+1):
+                if weights[row] > col:
+                    if row == 0:
+                        dp[row][col] = 0
+                    else:
+                        dp[row][col] = dp[row-1][col]
+                else:
+                    #num_fruits = col // weights[row]
+                    if row > 0:
+                        
+                        dp[row][col] = max(dp[row-1][col],dp[row-1][col-weights[row]] + profits[row])
+                    else:
+                        #this is basically the first row's calculation
+                        dp[row][col] = profits[row]
+
+        return dp[-1][-1]
+
+
+def main():
+    test = Solution()
+    #print(test.solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 5))
+    #print(test.solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6))
+    print(test.solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
+
+
+main()
+
