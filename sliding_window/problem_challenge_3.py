77c1c6c0c6b5b86d8c1f896bc1a66765f67c4f7d
John Ahn <Ahn_John@bah.com>
2022-03-23 14:13:06 -0400

    forgot to save

--- sliding_window/problem_challenge_3.py
+++ sliding_window/problem_challenge_3.py
@@ -1,18 +1,34 @@
+# Given a string and a pattern, find the smallest substring in the given string which has all the character occurrences of the given pattern.
 
 class Solution:
     def find_substring(self,str1, pattern):
         # TODO: Write your code here
 
-        lw,rw = 0, 1
+        lw,rw = 0,0
 
         h_map = {}
-        for char in str1:
+        for char in pattern:
             if char in h_map:
-                h_map[char] += 1
+                h_map[char]+=1
             else:
-                h_map[char] = 1
+                h_map[char]=1
+        
+        while lw<len(str1)-1:
+            if str1[rw] in h_map:
+                h_map[str1[rw]]-=1
+                if h_map[str1[rw]] == 0:
+                    del h_map[str1[rw]]
+            rw+=1
+            if len(h_map)==0:
+                while lw<rw:
+                    if str1[lw] not in h_map:
+                        lw+=1
+                    else:
+                        return str1[lw:rw+1]
 
-        while lw<len(str1) and len(h_map) > 0:
-            
 
         return ""
+
+
+test = Solution()
+print(test.find_substring())
