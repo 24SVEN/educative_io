b472838e5575e6a1015b235b15913815fc16ccd4
24SVEN <johnahn0124@gmail.com>
2022-04-07 01:16:17 -0400

    solved subsets using bitmasks

--- scrap.py
+++ scrap.py
@@ -1,19 +1,21 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        count = 0
+        result = 0
         count_r = 0
         count_l = 0
         for i in range(len(s)):
+            #012345678
+            #RLRRLLRLRL
             if s[i] == 'R':
                 count_r += 1
             elif s[i] == 'L':
                 count_l += 1
             
             if count_l == count_r:
-                count += 1
+                result += 1
                 count_r = 0
                 count_l = 0
-        return count
+        return result
 
 test = Solution()
 print(test.balancedStringSplit('RLRRLLRLRL')) #4
--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,16 +1,29 @@
 from collections import deque
 def find_subsets(nums):
-    subsets = []
-    # start by adding the empty subset
-    subsets.append([])
+    # subsets = []
+    # # start by adding the empty subset
+    # subsets.append([])
 
-    for num in nums:
-        for i in range(len(subsets)):
-            temp = list(subsets[i])
-            temp.append(num)
-            subsets.append(temp)
+    # for num in nums:
+    #     for i in range(len(subsets)):
+    #         temp = list(subsets[i])
+    #         temp.append(num)
+    #         subsets.append(temp)
 
-    return subsets
+    # return subsets
+    res = []
+
+    for i in range(1<<(len(nums))):
+        temp = []
+        for j in range(len(nums)):
+            print((i & (1<<j)),bin((i & (1<<j))))
+            if (i & (1<<j)):
+                temp.append(nums[j])
+        res.append(temp)
+
+    return res
+
+
 
 
 def main():
