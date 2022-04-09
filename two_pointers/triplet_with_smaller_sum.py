f24d9891b028dda942e53584139cf47d926b4af7
24SVEN <johnahn0124@gmail.com>
2022-04-09 17:11:04 -0400

    solved triplet with smaller sum

--- two_pointers/triplet_with_smaller_sum.py
+++ two_pointers/triplet_with_smaller_sum.py
@@ -1,41 +1,42 @@
-class Solution():
 
+
+class Solution:
     def triplet_with_smaller_sum(self,arr, target):
-        count = -1
-        triplets = 0
-        # TODO: Write your code here
+
+        lw,rw = 0,len(arr)-1
         arr.sort()
+        count = 0
+        while lw<rw:
+            temp_target = target-arr[lw]
 
-        for i in range(2,len(arr)):
-            triplets = self.find_triplet(arr,target,triplets,i)
+            count += self.find_pair(arr,temp_target,lw+1,rw)
+            lw += 1
+            
+
+        return count
 
 
-        return triplets
+    def find_pair(self,arr,target,lw,rw):
 
-    def find_triplet(self,arr,target,triplets,idx):
-        rw = idx - 1
-        lw = 0
+        count = 0
+        while lw<rw:
+            curr_sum = arr[lw] + arr[rw]
 
-        
-        while lw < rw:
-            temp_sum = arr[rw] + arr[lw] + arr[idx]
-            if temp_sum < target:
-                triplets += 1
-
-            if temp_sum <= target:
+            if curr_sum < target:
+                # since arr[right] >= arr[left], therefore, we can replace arr[right] by any number between left and right to get a sum less than the target sum
+                # For example, using the first test case, if all values in rw-lw are [3,2] and those added up with -1 will always be less than the target of 4
+                count += (rw-lw)
+                lw += 1
+            elif curr_sum >= target:
                 rw -= 1
 
-            else:
+        return count
                 
-                lw += 1
-        
-        return triplets
-
 
 test = Solution()
 print(test.triplet_with_smaller_sum([-1, 0, 2, 3],3)) #2
 
-#print(test.triplet_with_smaller_sum([-1, 4, 2, 1, 3],5)) #4
+print(test.triplet_with_smaller_sum([-1, 4, 2, 1, 3],5)) #4
 
 
 
